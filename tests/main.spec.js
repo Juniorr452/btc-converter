30b987f2eee7b2405136af07fcee1b5950b9f99f
Junior <juniorr452@gmail.com>
2019-07-29 13:14:29 -0300

    Commander e teste de versão

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,13 +1,14 @@
 const expect = require("chai").expect;
+const pkg = require("../package.json");
 
 const exec = require("child_process").exec;
 const btcConverter = "node .\\src\\main.js";
 
 describe("Main CLI", () => {
-  it("should return Hello World", (done) => {
-    exec(btcConverter, (err, stdout, stderr) => {
+  it("should return the current version", (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
       if(err) throw err;
-      expect(stdout.replace("\n", "")).to.be.equal("Hello World!");
+      expect(stdout.replace("\n", "")).to.be.equal(pkg.version);
       done();
     });
   });
